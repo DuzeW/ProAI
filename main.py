4320e2d12f673814a136f50eb91bcb62be3897f4
DuzeW <wiktor.wojcicki05@gmail.com>
2025-01-25 05:39:51 +0100

    Poprawa promptów teraz tłumacz się obraził. Może trzeba go usunąć

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
 from crew import TravelingCrew
 
 if __name__ == '__main__':
-    city = "Warsaw" #miasto
-    preferred_weather = "sunny" #Opcje deszcz słonecznie pochmurnie śnieg
+    city = "Gdansk" #miasto
     answer_language = "Polish"#Opcje Polski angielski japoński(dla preferencji ćwiczeniowca) front dajnie jak by też się zmieniał
-    budget = "middle"#High middle low
-    preferred_transportation = "private" #Opcje Public Private
-    point_of_interest = "business" #Niech sam sobie wpisze
-    preferred_accommodation = ""
+    budget = "High"#High middle low
+    preferred_transportation = "Private" #Opcje Public Private
+    point_of_interest = "History" #Niech sam sobie wpisze
+    preferred_accommodation = "Hotel"
     inputs = {
         "city": city,
-        "preferred_weather": preferred_weather,
         "answer_language": answer_language,
         "budget": budget,
         "preferred_transportation": preferred_transportation,
         "point_of_interest": point_of_interest,
-        "preferred_accommodation": preferred_weather,
+        "preferred_accommodation": preferred_accommodation,
     }
     TravelingCrew().crew().kickoff(inputs=inputs)
